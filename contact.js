59b49ac2c0ded0c70fc1cc0f59bf22b5a33d5fe9
Clayton Featherstone <cfeather@shdh-dhcp-114-020.wharton.upenn.edu>
2018-10-15 13:14:00 -0400

    Contacts and options modularized

---
+++ contact.js
@@ -0,0 +1,15 @@
+document.write("<img src=\"https://www.dropbox.com/s/i92emzyr00jjel5/PassportPhoto.jpg?dl=1\" width=\"240px\"></img>  ");
+document.write("<br />                                                                                                ");
+document.write("<p>                                                                                                   ");
+document.write("1352 Steinberg Hall-Dietrich Hall<br />                                                               ");
+document.write("3620 Locust Walk<br />                                                                                ");
+document.write("Philadelphia, PA 19104                                                                                ");
+document.write("<br />                                                                                                ");
+document.write("<br />                                                                                                ");
+document.write("<a href=\"https://bepp.wharton.upenn.edu/profile/claytonf/\">Wharton profile</a>                      ");
+document.write("<br />                                                                                                ");
+document.write("<br />                                                                                                ");
+document.write("<a class=\"email\" href=\"mailto:claytonf@wharton.upenn.edu\">claytonf@wharton.upenn.edu</a><br />    ");
+document.write("(214) 673-2800 <br />                                                                                 ");
+document.write("                                                                                                      ");
+document.write("</p>                                                                                                  ");
---
+++ options.js
@@ -0,0 +1,8 @@
+document.write("<br />                                                                                                           ");
+document.write("<a href=\"index.html\" class=\"navigation\">Research</a>                                                         ");
+document.write("<br />                                                                                                           ");
+document.write("<br />                                                                                                           ");
+document.write("<a href=\"teaching.html\" class=\"navigation\">Teaching</a>                                                      ");
+document.write("<br />                                                                                                           ");
+document.write("<br />                                                                                                           ");
+document.write("<a href=\"https://www.dropbox.com/s/su7mwywtqa0f5g0/CV.pdf?dl=1\" class=\"navigation\" target=\"_blank\">CV</a>  ");
