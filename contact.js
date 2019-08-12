e5bf4929a97d4b7a4b451b0f7d2f6cb1fc1a82f9
Clayton Featherstone <cfeather@gmail.com>
2019-08-12 12:00:24 -0400

    Changed adderss to Vance

--- contact.js
+++ contact.js
@@ -1,12 +1,9 @@
 document.write("<img src=\"headshot.jpg\" width=\"240px\"></img>  ");
 document.write("<br />                                                                                                ");
 document.write("<p>                                                                                                   ");
-document.write("1352 Steinberg Hall-Dietrich Hall<br />                                                               ");
-document.write("3620 Locust Walk<br />                                                                                ");
+document.write("319 Vance Hall<br />                                                               ");
+document.write("3733 Spruce St.<br />                                                                                ");
 document.write("Philadelphia, PA 19104                                                                                ");
-document.write("<br />                                                                                                ");
-document.write("<br />                                                                                                ");
-document.write("<a href=\"https://bepp.wharton.upenn.edu/profile/claytonf/\">Wharton profile</a>                      ");
 document.write("<br />                                                                                                ");
 document.write("<br />                                                                                                ");
 document.write("<a class=\"email\" href=\"mailto:claytonf@wharton.upenn.edu\">claytonf@wharton.upenn.edu</a><br />    ");
