ad6474f8b76257709007fbdcc8a88381bb78876c
Clayton Featherstone <cfeather@gmail.com>
2020-01-06 14:21:52 -0500

    Fixed broken CV link

--- options.js
+++ options.js
@@ -5,4 +5,4 @@ document.write("<br />                                                          
 document.write("<a href=\"teaching.html\" class=\"navigation\">Teaching</a>                                                      ");
 document.write("<br />                                                                                                           ");
 document.write("<br />                                                                                                           ");
-document.write("<a href=\"https://www.dropbox.com/s/cuxmyq1kwu1iasg/CV.pdf?dl=1\" class=\"navigation\" target=\"_blank\">CV</a>  ");
+document.write("<a href=\"https://www.dropbox.com/s/462w0p3ywun2jir/CV.pdf?dl=1\" class=\"navigation\" target=\"_blank\">CV</a>  ");
