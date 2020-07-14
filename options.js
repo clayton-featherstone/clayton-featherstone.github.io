8bca7d945d9f4306162ebf7378a7ee5569f15a75
Clayton Featherstone <cfeather@gmail.com>
2020-07-14 12:17:20 -0400

    Changed CV link from dl=1 to raw=1

--- options.js
+++ options.js
@@ -5,4 +5,4 @@ document.write("<br />                                                          
 document.write("<a href=\"teaching.html\" class=\"navigation\">Teaching</a>                                                      ");
 document.write("<br />                                                                                                           ");
 document.write("<br />                                                                                                           ");
-document.write("<a href=\"https://www.dropbox.com/s/vgnrd6r8ucbmirp/CV.pdf?dl=1\" class=\"navigation\" target=\"_blank\">CV</a>  ");
+document.write("<a href=\"https://www.dropbox.com/s/vgnrd6r8ucbmirp/CV.pdf?raw=1\" class=\"navigation\" target=\"_blank\">CV</a>  ");
