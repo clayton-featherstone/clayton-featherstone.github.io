e137c687f7ddb1038d07acaa3c6ead40fac16f84
Clayton Featherstone <cfeather@Claytons-MacBook-Pro-2.local>
2022-08-22 11:11:42 -0500

    Changed address to Baylor

--- contact.js
+++ contact.js
@@ -1,12 +1,13 @@
 document.write("<img src=\"headshot.jpg\" width=\"240px\"></img>  ");
 document.write("<br />                                                                                                ");
 document.write("<p>                                                                                                   ");
-document.write("319 Vance Hall<br />                                                               ");
-document.write("3733 Spruce St.<br />                                                                                ");
-document.write("Philadelphia, PA 19104                                                                                ");
+document.write("Baylor University<br />                                                               ");
+document.write("Hankamer School of Business<br />                                                               ");
+document.write("Department of Economics<br />                                                                                ");
+document.write("Foster 320.14                                                                                ");
 document.write("<br />                                                                                                ");
 document.write("<br />                                                                                                ");
-document.write("<a class=\"email\" href=\"mailto:claytonf@wharton.upenn.edu\">claytonf@wharton.upenn.edu</a><br />    ");
+document.write("clayton_featherstone@baylor.edu</a><br />    ");
 document.write("(214) 673-2800 <br />                                                                                 ");
 document.write("                                                                                                      ");
 document.write("</p>                                                                                                  ");
